3185599be5c1aa1754776de6959c7798b3dee31f
WEI, ZHIXIANG <54713447+w1oves@users.noreply.github.com>
2024-06-25 16:12:10 +0800

    Update dinov2_mask2former_512x512_bs1x4.py

--- configs/dinov2/dinov2_mask2former_512x512_bs1x4.py
+++ configs/dinov2/dinov2_mask2former_512x512_bs1x4.py
@@ -24,20 +24,20 @@ train_dataloader = dict(batch_size=4, dataset=dict(pipeline=train_pipeline))
 # in backbone
 embed_multi = dict(lr_mult=1.0, decay_mult=0.0)
 optim_wrapper = dict(
-    constructor="PEFTOptimWrapperConstructor",
     optimizer=dict(
-        type="AdamW", lr=0.0001, weight_decay=0.05, eps=1e-8, betas=(0.9, 0.999)
-    ),
+        type='AdamW',
+        lr=0.0001,
+        weight_decay=0.05,
+        eps=1e-08,
+        betas=(0.9, 0.999)),
     paramwise_cfg=dict(
-        custom_keys={
-            "norm": dict(decay_mult=0.0),
-            "query_embed": embed_multi,
-            "level_embed": embed_multi,
-            "learnable_tokens": embed_multi,
-            "reins.scale": embed_multi,
-        },
-        norm_decay_mult=0.0,
-    ),
+        custom_keys=dict(
+            norm=dict(decay_mult=0.0),
+            query_embed=dict(lr_mult=1.0, decay_mult=0.0),
+            query_feat=dict(lr_mult=1.0, decay_mult=0.0),
+            level_embed=dict(lr_mult=1.0, decay_mult=0.0),
+            backbone=dict(lr_mult=0.1)),
+        norm_decay_mult=0.0)
 )
 param_scheduler = [
     dict(type="PolyLR", eta_min=0, power=0.9, begin=0, end=40000, by_epoch=False)
